b230b5ad8dbf618d458240efc00dc0b5b0fd8956
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-24 15:08:11 -0300

    Configurando o session

--- tasks-app/src/pages/_app.tsx
+++ tasks-app/src/pages/_app.tsx
@@ -3,10 +3,13 @@ import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <>
-      <SessionProvider session={pageProps.session}>
+      <SessionProvider session={session}>
         <Header />
         <Component {...pageProps} />
       </SessionProvider>
