e54de4c96ace43f24da1212e48ef6837fcce83d7
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-24 13:40:10 -0300

    Styles Header component

--- tasks-app/src/components/header/index.tsx
+++ tasks-app/src/components/header/index.tsx
@@ -1,12 +1,19 @@
 import styles from './Header.module.css'
+import Link from 'next/link'
 
 export function Header() {
   return (
-    <header>
-      <section>
-        <nav>
-          <h1>Tarefas+</h1>
+    <header className={`${styles.header}`}>
+      <section className={`${styles.header__content}`}>
+        <nav className={`${styles.nav}`}>
+          <Link href="/">
+            <h1 className={`${styles.nav__logo}`}>
+              Tarefas<span>+</span>
+            </h1>
+          </Link>
         </nav>
+
+        <button className={`${styles.login}`}>Acessar</button>
       </section>
     </header>
   )
