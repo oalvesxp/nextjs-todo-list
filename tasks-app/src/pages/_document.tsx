ac26d65c7f96807ed5e028ddbf3481aacc0a9e61
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-25 09:11:37 -0300

    Separado as fonts em um arquivo

--- tasks-app/src/pages/_document.tsx
+++ tasks-app/src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { geistSans, geistMono } from './fonts'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Main />
         <NextScript />
       </body>
--- tasks-app/src/pages/index.tsx
+++ tasks-app/src/pages/index.tsx
@@ -1,18 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import localFont from 'next/font/local'
 import styles from '@/styles/Home.module.css'
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
 
 export default function Home() {
   return (
@@ -27,9 +15,7 @@ export default function Home() {
       </Head>
 
       {/** Body content */}
-      <div
-        className={`${styles.container} ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <div className={`${styles.container}`}>
         <main className={`${styles.main}`}>
           <div className={`${styles.logo}`}>
             <Image
