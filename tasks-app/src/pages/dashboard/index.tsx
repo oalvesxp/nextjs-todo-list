72f9d45af27476be07da90abe1f7d42b2b5e2325
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-10-25 09:44:50 -0300

    Formulário de publicação de tarefas

--- tasks-app/src/pages/dashboard/index.tsx
+++ tasks-app/src/pages/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import { GetServerSideProps } from 'next'
-import styles from '@/styles/Dashboard.module.css'
-import Head from 'next/head'
 import { authOptions } from '../api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth'
+import { TextArea } from '@/components/textarea'
+import styles from '@/styles/Dashboard.module.css'
+import Head from 'next/head'
 
 export default function Dashboard() {
   return (
@@ -11,7 +12,27 @@ export default function Dashboard() {
         <title>Tarefas+ | Meu painel de tarefas</title>
       </Head>
       <div className={`${styles.container}`}>
-        <h2>Dashboard</h2>
+        <main className={`${styles.main}`}>
+          <section className={`${styles.content}`}>
+            <div className={`${styles.content__form}`}>
+              <div className={`${styles.content__form}`}>
+                <h1 className={`${styles.content__form__title}`}>
+                  Qual sua tarefa?
+                </h1>
+                <form>
+                  <TextArea placeholder="Digite qual sua tarefa" />
+                  <div className={`${styles.content__form__checkboxArea}`}>
+                    <input type="checkbox" className={`${styles.checkbox}`} />
+                    <label>Deixar essa tarefa pública?</label>
+                  </div>
+                  <button type="submit" className={`${styles.button}`}>
+                    Publicar
+                  </button>
+                </form>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     </>
   )
